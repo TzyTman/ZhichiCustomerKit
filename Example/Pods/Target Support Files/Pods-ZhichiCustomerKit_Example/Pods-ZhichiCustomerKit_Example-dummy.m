c9ac04cabea40383f93188ba836af5ca107c4937
tuzhengyao <905430956@qq.com>
2022-11-30 13:31:57 +0800

    initial Commit of Library

---
+++ Example/Pods/Target Support Files/Pods-ZhichiCustomerKit_Example/Pods-ZhichiCustomerKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZhichiCustomerKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ZhichiCustomerKit_Example
+@end
---
+++ Example/Pods/Target Support Files/ZhichiCustomerKit/ZhichiCustomerKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZhichiCustomerKit : NSObject
+@end
+@implementation PodsDummy_ZhichiCustomerKit
+@end
